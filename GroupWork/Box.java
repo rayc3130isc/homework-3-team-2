1677c24eb49e4e4247f725e071ae651078bc4300
Tasnim Hasan <hasantasnim093@gmail.com>
2026-02-26 19:42:09 -0500

    Initaal commit

---
+++ GroupWork/Box.java
@@ -0,0 +1,21 @@
+package GroupWork;
+
+import java.util.ArrayList;
+
+public class Box<t> {
+t value;
+	
+Box  (t value){
+	this.value = value;
+}
+
+	 t getValue() {
+		 return value;
+	 }
+
+
+	
+	
+	
+	
+}
---
+++ GroupWork/Main.java
@@ -0,0 +1,45 @@
+package GroupWork;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ArrayList<Box<String>> names = new ArrayList<>();
+		ArrayList<Box<Integer>> ids = new ArrayList<>();		
+		
+	      Box<String> a = new Box<>("value");	
+	    
+	      
+	      
+	      Box<String> b = new Box<>("tasnim");
+	      Box<String> a0 = new Box<>("lebron");	
+	      Box<String> a90 = new Box<>("jordan");	
+	      Box<Integer> a1 = new Box<>(90); Box<Integer> a2 = new Box<>(28);
+	      Box<Integer> a3 = new Box<>(87);Box<Integer> a4 = new Box<>(100);
+	      names.add(a);names.add(b);names.add(a0);names.add(a90);names.add(a);
+	      ids.add(a1); ids.add(a2); ids.add(a3); ids.add(a4);
+	      Iterator<Box<String>> it = names.iterator();
+	      
+	      while(it.hasNext()) {
+	    	  Box<String> element = it.next();
+	    	  System.out.println(element.getValue());
+	      }
+	      
+	      Iterator<Box<Integer>> ite = ids.iterator();
+	      
+	      while(ite.hasNext()) {
+	    	  Box<Integer> element = ite.next();
+	    	  System.out.println(element.getValue());
+	      }
+	      
+	      
+	
+		
+		
+		
+		
+	}
+
+}
